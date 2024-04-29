e1673eb1c17e6b3da07a41f3ca8d997919b14bf0
joseeurquidi <josee.urquidi@gmail.com>
2024-04-28 20:26:21 -0600

    Famexso

--- pagina/assets/js/main.js
+++ pagina/assets/js/main.js
@@ -62,3 +62,29 @@ addEventListener("resize", (event) => {
     closeMenu()
   }
 });
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Scroll top button
+   */
+  const scrollTop = document.querySelector('.scroll-top');
+  if (scrollTop) {
+    const togglescrollTop = function() {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+    window.addEventListener('load', togglescrollTop);
+    document.addEventListener('scroll', togglescrollTop);
+    scrollTop.addEventListener('click', window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    }));
+  }
